1a785cadb0484ee6cd67bffabee255d73fa277c0
archaon <archaon@generative.xyz>
2023-04-20 15:05:46 +0700

    feat: update metadata image

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,5 +1,6 @@
-import { CDN_URL } from "@/configs";
+import { CDN_URL } from '@/configs';
 
 export const SEO_TITLE = 'Trustless Domain';
-export const SEO_DESCRIPTION = 'Name your TC wallet and use your BNS name to receive any token and NFT. No more copying and pasting long addresses.';
-export const SEO_IMAGE = `${CDN_URL}/images/display.jpg`;
+export const SEO_DESCRIPTION =
+  'Name your TC wallet and use your BNS name to receive any token and NFT. No more copying and pasting long addresses.';
+export const SEO_IMAGE = `${CDN_URL}/images/metadata-Trustless-Domain.jpg`;
