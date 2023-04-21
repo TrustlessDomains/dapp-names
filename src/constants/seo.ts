7a88a38c805e6f312ef30d7f51e3e9ea66dc6558
archaon <archaon@generative.xyz>
2023-04-21 10:15:49 +0700

    fix: update seo desc

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -2,5 +2,5 @@ import { CDN_URL } from '@/configs';
 
 export const SEO_TITLE = 'Trustless Domain';
 export const SEO_DESCRIPTION =
-  'Name your TC wallet and use your BNS name to receive any token and NFT. No more copying and pasting long addresses.';
+  'Use your BNS (Bitcoin Name System) name to receive any token and NFT. No more copying and pasting long addresses.';
 export const SEO_IMAGE = `${CDN_URL}/images/metadata-Trustless-Domain.jpg`;
